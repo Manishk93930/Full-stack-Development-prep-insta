76ccc1d0ed33f273938a4c3f266240e64517b58a
Manishk93930 <manishk93930@gmail.com>
2024-02-03 00:09:11 +0530

    week 3 project 2 added

---
+++ Image Gallery/img.js
@@ -0,0 +1,37 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    let uploadInput = document.getElementById('upload1');
+    let gallery = document.querySelector('.gallery');
+
+    uploadInput.addEventListener('change', function () {
+        if (uploadInput.files.length > 0) {
+            let file = uploadInput.files[0];
+
+            // Create an object URL for the selected file
+            let objectURL = URL.createObjectURL(file);
+
+            // Create a new image element
+            let newImage = new Image();
+            newImage.src = objectURL;
+
+            // Append the new image to the gallery
+            gallery.appendChild(newImage);
+
+            // Release the object URL to free resources
+          //   URL.revokeObjectURL(objectURL);
+        }
+    });
+
+    let arrowbtn=document.querySelector(".arrow");
+    arrowbtn.onclick = () => {
+     var active = document.querySelector(".active");
+     if(gallery.classList.contains("active")){
+          gallery.classList.remove("active");
+     }else{
+          gallery.classList.add("active");
+     }
+ 
+    }
+});
+
+

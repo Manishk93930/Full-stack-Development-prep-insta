bf108b787a27dcc0d97e34f93a4fbaf9925aee65
Manishk93930 <139479598+Manishk93930@users.noreply.github.com>
2024-03-04 22:45:07 +0530

    Add files via upload

---
+++ controllers/foodController.js
@@ -0,0 +1,66 @@
+const Food = require('../models/Food');
+
+// Create a new food item
+exports.createFood = async (req, res) => {
+  try {
+    const { name, foodGroup, description } = req.body;
+    if (!name || !foodGroup || !description) {
+      return res.status(400).json({ error: 'Name, foodGroup, and description are required fields' });
+    }
+
+    const food = new Food(req.body);
+    await food.save();
+    res.status(201).json(food);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Get all food items
+exports.getAllFood = async (req, res) => {
+  try {
+    const foods = await Food.find();
+    res.json(foods);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Get a specific food item by ID
+exports.getFoodById = async (req, res) => {
+  try {
+    const food = await Food.findById(req.params.id);
+    if (!food) {
+      return res.status(404).json({ error: 'Food not found' });
+    }
+    res.json(food);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Update a food item
+exports.updateFood = async (req, res) => {
+  try {
+    const food = await Food.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!food) {
+      return res.status(404).json({ error: 'Food not found' });
+    }
+    res.json(food);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Delete a food item
+exports.deleteFood = async (req, res) => {
+  try {
+    const food = await Food.findByIdAndDelete(req.params.id);
+    if (!food) {
+      return res.status(404).json({ error: 'Food not found' });
+    }
+    res.json(food);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
